4ef83798449692b1c3488e6ddbcab311ce9ba8c4
GaurangPatel <gp853212@gmail.com>
2021-11-08 22:58:44 +0530

    add icons

--- Components/home/Header.js
+++ Components/home/Header.js
@@ -1,22 +1,38 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, Image, SafeAreaView, TouchableOpacity } from 'react-native'
 
 const Header = () => {
     return (
-        <View>
-            <Image style={styles.logo} source={require('../../assets/logo.png')}/> 
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <Image style={styles.logo} source={require('../../assets/logo.png')} />
+            </TouchableOpacity>
+
+
+            <View style={styles.iconsContainer}>
+                <Text style={{ color: 'white' }}>Dummy</Text>
+                <Text style={{ color: 'white' }}>Dummy</Text>
+                <Text style={{ color: 'white' }}>Dummy</Text>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-
+    container: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginHorizontal: 20,
     },
-    logo:{
-        height:100,
-        width:150,
-        }
+    logo: {
+        height: 100,
+        width: 150,
+        resizeMode: 'contain',
+    },
+    iconsContainer: {
+        flexDirection: 'row'
+    }
 })
 
 export default Header
