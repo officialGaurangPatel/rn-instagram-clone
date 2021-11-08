ddf8dc39d94bac03b9fdb787c723dabbc6a37aec
GaurangPatel <gp853212@gmail.com>
2021-11-09 00:39:13 +0530

    Add Story Component

---
+++ Components/data/users.js
@@ -0,0 +1,38 @@
+export const USERS = [
+    {
+        user: 'parth Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'karan Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'rahul Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'mehul Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'Gaurang Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'priyansh Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'sanket Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'sanket Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'sanket Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    }
+]
--- Components/home/Stories.js
+++ Components/home/Stories.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
+import { USERS } from '../data/users'
 const Stories = () => {
     return (
-        <View>
-            <Text style={{ color: 'white' }}>Stories</Text>
+        <View style={{ marginBottom: 13 }}>
+            <ScrollView horizontal showsVerticalScrollIndicator={false}>
+                {USERS.map((story, index) => (
+                    <View key={index} style={{ alignItems: 'center', cursor: 'pointer' }}>
+                        <Image source={{ uri: story.image }} style={styles.story} />
+                        <Text style={{ color: 'white' }}>{story.user.length > 11 ? story.user.slice(0, 10).toLowerCase() + '...' : story.user.toLowerCase()}</Text>
+                    </View>
+                ))}
+            </ScrollView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    story: {
+        height: 70,
+        width: 70,
+        borderRadius: 50,
+        marginLeft: 6,
+        borderWidth: 3,
+        borderColor: '#ff8501'
+    }
+})
 
 export default Stories
