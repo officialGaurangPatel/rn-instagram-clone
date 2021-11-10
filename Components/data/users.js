de790e6aea76e98ce7484d631dba86285f2e2382
GaurangPatel <gp853212@gmail.com>
2021-11-10 23:02:12 +0530

    add more stories

--- Components/data/users.js
+++ Components/data/users.js
@@ -34,5 +34,53 @@ export const USERS = [
     {
         user: 'sanket Patel',
         image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'parth Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'karan Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'rahul Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'mehul Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'Gaurang Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'priyansh Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'mehul Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'Gaurang Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'priyansh Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'sanket Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'sanket Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
+    },
+    {
+        user: 'sanket Patel',
+        image: 'https://i.imgur.com/dxjd7FB.jpg',
     }
 ]
