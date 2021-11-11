cad7817658ace560db605e382e438569def0cf96
GaurangPatel <gp853212@gmail.com>
2021-11-11 23:57:30 +0530

    make changes

--- Components/newPost/FormikPostUploader.js
+++ Components/newPost/FormikPostUploader.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { View, Text, Image, TextInput, Button } from 'react-native'
 import * as Yup from 'yup'
 import { Formik } from 'formik'
+import validUrl from 'valid-url'
 
 const placeholder_Img = 'https://image.shutterstock.com/image-vector/error-500-page-empty-symbol-260nw-1711106146.jpg'
 
@@ -10,18 +11,21 @@ const uploadPostSchema = Yup.object().shape({
     caption: Yup.string().max(2200, 'Caption has reached the characters')
 })
 
-const FormikPostUploader = () => {
+const FormikPostUploader = ({ navigation }) => {
     const [thumbnailUrl, setThumbnailUrl] = useState(placeholder_Img)
     return (
         <Formik
             initialValues={{ caption: '', imageUrl: '' }}
-            onSubmit={(values) => console.log(values)}
+            onSubmit={(values) => {
+                console.log(values)
+                navigation.push('HomeScreen')
+            }}
             validationSchema={uploadPostSchema}
             validateOnMount={true}>
             {({ handleBlur, handleChange, handleSubmit, values, errors, isValid }) =>
                 <>
                     <View style={{ margin: 20, justifyContent: 'space-between', flexDirection: 'row' }}>
-                        <Image style={{ width: 100, height: 90 }} source={{ uri: thumbnailUrl ? thumbnailUrl : placeholder_Img }} />
+                        <Image style={{ width: 100, height: 90 }} source={{ uri: validUrl.isUri(thumbnailUrl) ? thumbnailUrl : placeholder_Img }} />
                         <View style={{ flex: 1, marginLeft: 12 }}>
                             <TextInput
                                 placeholder='Write a caption...'
