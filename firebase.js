378524ac6afd8a47e283ede512d782b8a7428a97
GaurangPatel <gp853212@gmail.com>
2021-11-16 23:54:26 +0530

    add firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBQxUyo-YhnlbqthX_WM6jNwhXX_zcmSUE",
+    authDomain: "rn-instagram-clone-6effd.firebaseapp.com",
+    projectId: "rn-instagram-clone-6effd",
+    storageBucket: "rn-instagram-clone-6effd.appspot.com",
+    messagingSenderId: "164239475352",
+    appId: "1:164239475352:web:de6d8b7625895fc43746ed"
+};
+
+const app = initializeApp(firebaseConfig)
+
+export default app
